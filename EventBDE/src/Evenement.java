7b9c8162ea1a80ccc08ef4a10f71318e5730335e
Cyprien Boree <cyprien.boree>
2016-09-01 08:33:20 +0200

    Creation attributes + getters/setters Evenement

--- EventBDE/src/Evenement.java
+++ EventBDE/src/Evenement.java
@@ -9,5 +9,42 @@
  * @author boreec
  */
 public class Evenement {
+    private String nom;
+    private Calendar dateDebut;
+    private Calendar dateFin;
+    private String description;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public Calendar getDateDebut() {
+        return dateDebut;
+    }
+
+    public Calendar getDateFin() {
+        return dateFin;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setDateDebut(Calendar dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public void setDateFin(Calendar dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
     
 }
