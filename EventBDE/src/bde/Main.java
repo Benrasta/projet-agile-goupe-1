0369116eadc9f0eb419e52caf93550cd91c31074
laudrey457@gmail.com <lorietta@ayou14.iut-infobio.priv.univ-lille1.fr>
2016-09-01 13:57:10 +0200

    evenement test

--- EventBDE/src/bde/Main.java
+++ EventBDE/src/bde/Main.java
@@ -5,8 +5,6 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
 
-//Audrey
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
