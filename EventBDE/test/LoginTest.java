1e612d77ab94a9055bc0a30dadf1acde7f49efa1
avanessm <avanessm@ayou07.iut-infobio.priv.univ-lille1.fr>
2016-09-01 09:43:39 +0200

    test login

--- EventBDE/test/LoginTest.java
+++ EventBDE/test/LoginTest.java
@@ -35,58 +35,10 @@ public class LoginTest {
     public void tearDown() {
     }
 
-    /**
-     * Test of getpseudo method, of class Login.
-     */
     @Test
-    public void testGetpseudo() {
-        System.out.println("getpseudo");
-        Login instance = null;
-        String expResult = "";
-        String result = instance.getpseudo();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testVerifier() {
+        Login login = new Login("admin", "admin");
+        boolean bol = false;
+        assertTrue(bol);
     }
-
-    /**
-     * Test of getmdp method, of class Login.
-     */
-    @Test
-    public void testGetmdp() {
-        System.out.println("getmdp");
-        Login instance = null;
-        String expResult = "";
-        String result = instance.getmdp();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setPseudo method, of class Login.
-     */
-    @Test
-    public void testSetPseudo() {
-        System.out.println("setPseudo");
-        String np = "";
-        Login instance = null;
-        instance.setPseudo(np);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setmdp method, of class Login.
-     */
-    @Test
-    public void testSetmdp() {
-        System.out.println("setmdp");
-        String nmdp = "";
-        Login instance = null;
-        instance.setmdp(nmdp);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
