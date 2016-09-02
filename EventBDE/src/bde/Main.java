bb1d935fdbc30af7b66e6b8a662ec18988bf5706
timothe59 <timothe.ruc@gmail.com>
2016-09-02 09:28:48 +0200

    Add files via upload

--- EventBDE/src/bde/Main.java
+++ EventBDE/src/bde/Main.java
@@ -49,12 +49,28 @@ public  class Main {
          System.out.println("2 : se connecter ");
          System.out.println("3 : quitter ");
          Scanner sctmp = new Scanner(System.in);
+        
          int cl = sctmp.nextInt();
          sctmp.nextLine();
+        
          if( cl == 1){
              le.trierList();
-             le.afficherCSV();
-         }else {
+             System.out.println(le.toString());
+             System.out.println("\nQue voulez-vous faire ?");
+             System.out.println("\n1 - visualiser la description d'un evenement");
+             System.out.println("2 - revenir au menu");
+             
+            int ck = sctmp.nextInt();
+            sctmp.nextLine();
+             
+            if(ck == 1 ){
+                System.out.println("Entrez le numero d'un evenement");
+                int cn = sctmp.nextInt();
+                sctmp.nextLine();
+                
+                System.out.println(le.liste.get(cn-1).avecDescription());
+            }
+        }else {
              if(cl == 2){
              System.out.println("pseudo : ");
              String pseu =sctmp.nextLine();
@@ -75,153 +91,162 @@ public  class Main {
              
              if(log.verifier()){
                  while (true){
-         le.trierList();
-         System.out.println("que voulez vous faire ?");
-         System.out.println("1 :afficher les evenements ");
-         System.out.println("2 : ajouter un evenement");
-         System.out.println("3 : supprimer un evenement ");
-         System.out.println("4 : modifier un evenement ");
-         System.out.println("5 : Visualiser un evenement ");
-         System.out.println("6 : quitter ");
+                    le.trierList();
+                    System.out.println("que voulez vous faire ?");
+                    System.out.println("1 : afficher les evenements ");
+                    System.out.println("2 : ajouter un evenement");
+                    System.out.println("3 : supprimer un evenement ");
+                    System.out.println("4 : modifier un evenement ");
+                    System.out.println("5 : creer sondage ");
+                    System.out.println("6 : quitter ");
+
          
-         
-         int cm =sctmp.nextInt(); 
-         sctmp.nextLine();         
-         if(cm == 1){
-               le.afficherCSV();
-                System.out.println("appuyer sur entrée pour continuer");
-         }else if(cm == 2){
-                  System.out.println(" voulez vous créer vraiment un évenement ?");
-           System.out.println("1 :oui ");
-            System.out.println("2 :non ");
-       
-         
-        
-            int r =sctmp.nextInt(); 
-         
-              sctmp.nextLine();
-                if(r == 1){
-                    System.out.println("entrer le nom de l'evenement");
-             
-           
-                    String name = sctmp.nextLine();
-                    System.out.println("appuyer sur entrée pour continuer"); 
+                    int cm =sctmp.nextInt(); 
                     sctmp.nextLine();
-           
-                    Evenement e3 = new Evenement(name);
-                    le.ajouterEvenement(e3);
-                   le.afficherCSV();
-                     
-                }
-                
-                
-             }else if(cm ==3 ){
-                      System.out.println(" voulez vous  vraiment supprimer un  évenement ?");
-         System.out.println("1 :oui ");
-         System.out.println("2 :non ");
-              
-         int r2 =sctmp.nextInt(); 
-         
-        sctmp.nextLine();
-         if(r2 == 1){
-             System.out.println("entrer le nom de l'evenement");
-             
-           
-               String name = sctmp.nextLine();
-              // sctmp.nextLine();
-               System.out.println(name);
-               
-            
-             le.supprimerEvenementAvecLeNom(name);
-            le.afficherCSV();
-              System.out.println("appuyer sur entrée pour continuer");
-         }
-         
-         
-                 }else if(cm == 4){
-                     System.out.println(le.toString());
-                     System.out.println("Quelle événement voulait vous modifier?");
-                     String name = sctmp.nextLine();
-                     if(le.nomDejaPresent(name)){
-                         System.out.println("1: Modifier le nom");
-                         System.out.println("2: Modifier la date de début (aaaa,m,jj)");
-                         System.out.println("3: Modifier la date de fin (aaaa,m,jj)");
-                         System.out.println("4: Modifier la description");
-                         
-                         
-                         
-                         int choix = sctmp.nextInt();
-                         sctmp.nextLine();
-                         if(choix == 1){
-                             System.out.println("Saisissez un nouveau nom :");
-                             String nom = sctmp.nextLine();
-                             le.retourneEvenement(name).setNom(nom);
-                         }else if(choix == 2){
-                             System.out.println("Saisissez l'année :");
-                             int aaaa = sctmp.nextInt();
-                             System.out.println("Saisissez le mois :");
-                             int m = sctmp.nextInt() -1;
-                             System.out.println("Saisissez le jour :");
-                             int jj = sctmp.nextInt();
-                             le.retourneEvenement(name).setDateDebut(new GregorianCalendar(aaaa,m,jj));
-                             le.afficherCSV();
-                         }else if(choix == 3){
-                             System.out.println("Saisissez l'année :");
-                             int aaaa = sctmp.nextInt();
-                             System.out.println("Saisissez le mois :");
-                             int m = sctmp.nextInt() -1;
-                              System.out.println("Saisissez le jour :");
-                             int jj = sctmp.nextInt();
-                             le.retourneEvenement(name).setDateFin(new GregorianCalendar(aaaa,m,jj));
-                             le.afficherCSV();
-                         }else if(choix == 4){
-                             System.out.println("Entrée une nouvelle description : ");
-                             String description = sctmp.nextLine();
-                             le.retourneEvenement(name).setDescription(description);
-                         }
-                     }else{
-                         System.out.println("Evénement introuvable !!");
-                     }
-                     
-                         
-                     }else if (cm == 5){
+                    
+                    if(cm == 1){
+
+                        le.trierList();
                         System.out.println(le.toString());
-                        System.out.println("Quelle événement voulait vous visualiser?");
+                        System.out.println("\nQue voulez-vous faire ?");
+                        System.out.println("\n1 - visualiser la description d'un evenement");
+                        System.out.println("2 - revenir au menu");
+
+                        int ck = sctmp.nextInt();
+                        sctmp.nextLine();
+
+                        if(ck == 1 ){
+                            System.out.println("Entrez le numero d'un evenement");
+                            int cn = sctmp.nextInt();
+                            sctmp.nextLine();
+                            
+                            if(cn>0 && cn<=le.liste.size())
+                                System.out.println(le.liste.get(cn-1).avecDescription());
+                            else
+                                System.out.println("Numero de l'evenement invalide\n");
+                        }
+                       System.out.println("appuyer sur entrée pour continuer");
+                    }
+                    else if(cm == 2){
+                       System.out.println(" voulez vous créer vraiment un évenement ?");
+                       System.out.println("1 :oui ");
+                       System.out.println("2 :non ");
+                
+                        int r =sctmp.nextInt();          
+                        sctmp.nextLine();
+                        
+                        if(r == 1){
+                            System.out.println("entrer le nom de l'evenement");
+                        
+                            String name = sctmp.nextLine();
+                            System.out.println("appuyer sur entrée pour continuer"); 
+                            sctmp.nextLine();
+
+                            Evenement e3 = new Evenement(name);
+                            le.ajouterEvenement(e3);
+                            le.afficherCSV();
+                        }                                
+                    }
+                     else if(cm ==3 ){
+                        System.out.println(" voulez vous  vraiment supprimer un  évenement ?");
+                        System.out.println("1 :oui ");
+                        System.out.println("2 :non ");
+              
+                        int r2 =sctmp.nextInt();          
+                        sctmp.nextLine();
+                        
+                        if(r2 == 1){
+                            System.out.println("entrer le nom de l'evenement");                        
+                            String name = sctmp.nextLine();
+                            // sctmp.nextLine();
+                            System.out.println(name);
+                           
+                            le.supprimerEvenementAvecLeNom(name);
+                            le.afficherCSV();
+                            System.out.println("appuyer sur entrée pour continuer");
+                        }                 
+                    }else if(cm == 4){
+                        
+                        System.out.println(le.toString());
+                        System.out.println("Quelle événement voulait vous modifier?");
                         String name = sctmp.nextLine();
                         
-                        System.out.println(le.retourneEvenement(name).toString()+"\n"+le.retourneEvenement(name).getDescription());
-                    }else if (cm == 6){
-                             System.exit(0);
-                         }
-         
-          sctmp.nextLine();
-                     }
-                 
-                 
-                 
-             }
+                        if(le.nomDejaPresent(name)){
+                            System.out.println("1: Modifier le nom");
+                            System.out.println("2: Modifier la date de début (aaaa,m,jj)");
+                            System.out.println("3: Modifier la date de fin (aaaa,m,jj)");
+                            System.out.println("4: Modifier la description");                                                  
+                         
+                            int choix = sctmp.nextInt();
+                            sctmp.nextLine();
+                            if(choix == 1){
+                                
+                                System.out.println("Saisissez un nouveau nom :");
+                                String nom = sctmp.nextLine();
+                                le.retourneEvenement(name).setNom(nom);
+                            }else if(choix == 2){
+                                
+                                System.out.println("Saisissez l'année :");
+                                int aaaa = sctmp.nextInt();
+                                System.out.println("Saisissez le mois :");
+                                int m = sctmp.nextInt() -1;
+                                System.out.println("Saisissez le jour :");
+                                int jj = sctmp.nextInt();
+                                le.retourneEvenement(name).setDateDebut(new GregorianCalendar(aaaa,m,jj));
+                                le.afficherCSV();
+                            }else if(choix == 3){
 
-             }else{
-                 if(cl == 3){
-                     System.exit(0);
-                 }
-             }
-         }
-         
-         
-         
-         
-         
-         
-                 }
-             }
+                                System.out.println("Saisissez l'année :");
+                                int aaaa = sctmp.nextInt();
+                                System.out.println("Saisissez le mois :");
+                                int m = sctmp.nextInt() -1;
+                                 System.out.println("Saisissez le jour :");
+                                int jj = sctmp.nextInt();
+                                le.retourneEvenement(name).setDateFin(new GregorianCalendar(aaaa,m,jj));
+                                le.afficherCSV();
+                            }else if(choix == 4){
+
+                                System.out.println("Entrée une nouvelle description : ");
+                                String description = sctmp.nextLine();
+                                le.retourneEvenement(name).setDescription(description);
+                            }
+                        }else{
+                            System.out.println("Evénement introuvable!");
+                     }                                              
+                     
+                    }
+                     else if(cm == 5){
+                        System.out.println(" voulez vous créer vraiment un sondage ?");                        
+                        System.out.println("1 :oui ");
+                        System.out.println("2 :non ");
+       
+                        int r =sctmp.nextInt();          
+                        sctmp.nextLine();
+
+                        if(r == 1){
+                            System.out.println("entrer le nom du sondage");                        
+                            String name = sctmp.nextLine();
+                            System.out.println("appuyer sur entrée pour continuer"); 
+                            sctmp.nextLine();
+
+                            Sondage e3 = new Sondage(name);
+                            le.ajouterEvenement(e3);                        
+                            System.out.println(le);
+                        } 
+                    }
+                    else if (cm == 6){
+                        System.exit(0);
+                    }
+
+                    sctmp.nextLine();
+                }                                                   
+            }
+        }else{
+            if(cl == 3)
+                System.exit(0);
+
+            }
+        }
+        }
+    }
 }
-         
-        
-     
-          
-         
-         
-        
-        
-    
